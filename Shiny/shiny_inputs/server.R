f7bd5ee85eace806011fa8a8436e47cfdeaca8d9
Daniel Behar Aldana <danielbehar@ufm.edu>
2022-08-10 18:32:34 -0600

    Agregando elementos extra

--- Shiny/shiny_inputs/server.R
+++ Shiny/shiny_inputs/server.R
@@ -5,9 +5,42 @@ shinyServer(function(input, output) {
   output$out_numeric_input = renderPrint({
     print(input$ninput)
     
+  })
+  
   output$out_slider_input = renderPrint({
     print(input$slinput)
-    })
+  })
+  
+  output$out_slider_input_multi = renderPrint({
+    print(input$slinputmulti)
+  })
+  
+  output$out_slider_input_ani = renderPrint({
+    print(input$slinputanimate)
+  })
+  
+  output$out_date_input = renderPrint({
+    print(input$date_input)
+  })
+  
+  output$out_date_input_range = renderPrint({
+    print(input$date_range_input)
+  })
+  
+  output$select_input = renderPrint({
+    print(input$select_input)
+  })
+  
+  output$select_input_2 = renderPrint({
+    print(input$select_input_2)
+  })
+  
+  output$checkbox_output = renderPrint({
+    print(input$chbox_input)
+  })
+  
+  output$checkbox_output_2 = renderPrint({
+    print(input$cchbox_group_input)
   })
   
 })
--- Shiny/shiny_inputs/ui.R
+++ Shiny/shiny_inputs/ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(lubridate)
 
 shinyUI(fluidPage(
 
@@ -10,7 +11,31 @@ shinyUI(fluidPage(
            numericInput("ninput", "Ingrese un numero:", value = 10),
            
            sliderInput('slinput', "Seleccione un porcentaje: ", min = 0, 
-          max = 100, step = 1, value = 0, post = '%')
+           max = 100, step = 1, value = 0, post = '%'),
+          
+           sliderInput('slinputmulti', "Seleccione un rango: ", value = c(
+           10000,30000), min = 0, max = 150000, step = 1000, pre = "Q",
+           sep = ','),
+           
+           sliderInput('slinputanimate', "Pasos: ", value = 0, min = 0, max = 100,
+           step = 10, animate = TRUE),
+           
+           dateInput('date_input', "Ingrese Fecha: ", value = Sys.Date(), 
+           language = 'es', weekstart = 1, format = 'dd-mm-yyyy'),
+           
+           dateRangeInput('date_range_input', "Seleccione Fechas: ", 
+           start = today()-15, end = today(), max = today(), min = today()-365,
+           language = 'es', weekstart = 1, separator = 'hasta'),
+           
+           selectInput('select_input', "Seleccione estado: ", choices = state.name,
+           selected = state.name[sample(1:length(state.name), size = 1)]),
+           
+           selectInput('select_input_2', "Seleccione letras: ", choices = letters,
+           selected = 'A', multiple = TRUE),
+           
+           checkboxInput('chbox_input', "Enviar email", value = FALSE),
+           checkboxGroupInput('cchbox_group_input', "Seleccione opciones: ",
+           choices = letters[1:4], selected = NULL)
         ),
 
         mainPanel(
@@ -18,7 +43,27 @@ shinyUI(fluidPage(
           h2("Numeric Input"),
           verbatimTextOutput('out_numeric_input'),
           h2("Slider Input"),
-          verbatimTextOutput('out_slider_input')
+          h3('Slider Input Simple'),
+          verbatimTextOutput('out_slider_input'),
+          h3('Slider Input Multiple'),
+          verbatimTextOutput('out_slider_input_multi'),
+          h3('Slider Input Animated'),
+          verbatimTextOutput('out_slider_input_ani'),
+          h2("Date Input"),
+          h3("Normal Date"),
+          verbatimTextOutput('out_date_input'),
+          h3("Date Range"),
+          verbatimTextOutput('out_date_input_range'),
+          h2("Select Input"),
+          h3("State only one"),
+          verbatimTextOutput('select_input'),
+          h3("More Letters"),
+          verbatimTextOutput('select_input_2'),
+          h2("Checkbox"),
+          h3("Email"),
+          verbatimTextOutput('checkbox_output'),
+          h3("Letras"),
+          verbatimTextOutput('checkbox_output_2')
         )
     )
 ))
