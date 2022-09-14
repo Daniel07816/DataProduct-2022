61e620f572bf35e609bff70e06bfc1459d0c806f
Daniel Behar Aldana <danielbehar@ufm.edu>
2022-09-13 21:36:27 -0600

    laboratorio finalizado al 90%

--- Shiny/plots_interaction/Laboratorio/server.R
+++ Shiny/plots_interaction/Laboratorio/server.R
@@ -8,7 +8,9 @@ output$grafica_base_r <- renderPlot({
   plot(mtcars$wt,mtcars$mpg, xlab = "wt", ylab="millas por galon")
 })
 
-punto <- reactiveValues()
+direccion <<- reactiveVal()
+puntos <<- reactiveValues()
+punto <<- reactiveVal()
 
 output$click_data <- renderPrint({
   clk_msg <- NULL
@@ -17,18 +19,27 @@ output$click_data <- renderPrint({
   mbrush_msg <- NULL
   if(!is.null(input$clk$x) ){
     clk_msg<-
-    paste0("click cordenada x= ", round(input$clk$x,2), 
+    paste0("click cordenada x= ", round(input$clk$x,2),
            " click coordenada y= ", round(input$clk$y,2))
-    punto$x <- round(input$clk$x,2)
-    punto$y <- round(input$clk$y,2)
+    posible = nearPoints(mtcars,input$clk,xvar='wt',yvar='mpg')
+    if(nrow(posible) != 0){
+      direccion <- toString(posible$wt-posible$mpg)
+      puntos[[direccion]] <- posible
+    }
   }
   if(!is.null(input$dclk$x) ){
     dclk_msg<-paste0("doble click cordenada x= ", round(input$dclk$x,2), 
                  " doble click coordenada y= ", round(input$dclk$y,2))
+    posible = nearPoints(mtcars,input$dclk,xvar='wt',yvar='mpg')
+    if(nrow(posible) != 0){
+      direccion <- toString(posible$wt-posible$mpg)
+      puntos[[direccion]] <- NULL
+    }
   }
   if(!is.null(input$mhover$x) ){
     mhover_msg<-paste0("hover cordenada x= ", round(input$mhover$x,2), 
                      " hover coordenada y= ", round(input$mhover$y,2))
+    punto <- nearPoints(mtcars,input$mhover,xvar='wt',yvar='mpg')
   }
   
   
@@ -36,10 +47,13 @@ output$click_data <- renderPrint({
     brushx <- paste0(c('(',round(input$mbrush$xmin,2),',',round(input$mbrush$xmax,2),')'),collapse = '')
     brushy <- paste0(c('(',round(input$mbrush$ymin,2),',',round(input$mbrush$ymax,2),')'),collapse = '')
     mbrush_msg <- cat('\t rango en x: ', brushx,'\n','\t rango en y: ', brushy)
+    
+    posibles = brushedPoints(mtcars,input$mbrush,xvar='wt',yvar='mpg')
+    if(nrow(posibles) != 0){
+      direccion <- toString(posibles$wt-posibles$mpg)
+      puntos[[direccion]] <- posibles
     }
-    
-    
-  
+    }
   
   cat(clk_msg,dclk_msg,mhover_msg,mbrush_msg,sep = '\n')
   
@@ -47,25 +61,32 @@ output$click_data <- renderPrint({
 
 
 
+output$plot_click_options <- renderPlot({
+  lista <<- reactiveValuesToList(puntos)
+  plot(mtcars$wt,mtcars$mpg, xlab = "wt", ylab="millas por galon")
+  points(punto()$wt,punto()$mpg,col='gray',pch=19)
+  lapply(lista, impresor)
+})
+
 output$mtcars_tbl <- renderTable({
- ## df <- nearPoints(mtcars,input$clk,xvar='wt',yvar='mpg')
-  df <- brushedPoints(mtcars,input$mbrush,xvar='wt',yvar='mpg')
+  df <- nearPoints(mtcars,input$clk,xvar='wt',yvar='mpg')
+  df2 <- brushedPoints(mtcars,input$mbrush,xvar='wt',yvar='mpg')
   if(nrow(df)!=0){
     df
   } else {
     NULL
   }
   
-})
-
-
-
-output$plot_click_options <- renderPlot({
-  
-  plot(mtcars$wt,mtcars$mpg, xlab = "wt", ylab="millas por galon")
-  points(mtcars$wt[punto$x],mtcars$mpg[punto$y],col='green',pch=19)
+  if (nrow(df2) != 0){
+    df2
+  } else {
+    NULL
+  }
   
 })
 
+impresor <- function(list){
+  points(list$wt,list$mpg,col='green',pch=19)
+}
 
 })
--- Shiny/plots_interaction/Laboratorio/ui.R
+++ Shiny/plots_interaction/Laboratorio/ui.R
@@ -5,7 +5,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Graficas y mas"),
+    titlePanel("Laboratorio"),
 
     # Sidebar with a slider input for number of bins
     shiny::tabsetPanel(
